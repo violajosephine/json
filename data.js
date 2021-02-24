984b2ae2d6f3f92072a8b3376a147c233acd5cc2
Anna W. Fleischer <anna802w@stud.kea.dk>
2021-02-24 10:40:08 +0100

    fetched both data - js ready for clone/populate

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 const url = "https://semester02t7-00e2.restdb.io/rest/breathe-text?max=4";
+const url1 = "https://semester02t7-00e2.restdb.io/rest/breathe-advice?max=4";
 
 //The API-Key:
 
@@ -23,3 +24,29 @@ fetch(url, options)
   .catch((e) => {
     console.error("An Error Occured:", e.message);
   });
+
+fetch(url1, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((data1) => {
+    //we have the data
+    console.log(data1);
+    //handle the data
+  })
+  .catch((e) => {
+    console.error("An Error Occured:", e.message);
+  });
+
+  function handleData(breathe) {
+      breathe.forEach(emotion) => {
+  }
+//make a template
+//grab it 
+//clone it 
+//populate with data
+//append it
+}
