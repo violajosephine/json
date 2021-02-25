087922dd077d7ba5d6d385ab278c9b0923d65689
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-02-25 01:29:03 +0100

    it works!!! hallelujah

--- data.js
+++ data.js
@@ -1,5 +1,55 @@
-const url = "https://semester02t7-00e2.restdb.io/rest/breathe-text?max=4";
-const url1 = "https://semester02t7-00e2.restdb.io/rest/breathe-advice?max=4";
+// const url = "https://semester02t7-00e2.restdb.io/rest/breathe-text?max=4";
+// const url1 = "https://semester02t7-00e2.restdb.io/rest/breathe-advice?max=4";
+
+// //The API-Key:
+
+// const options = {
+//   headers: {
+//     "x-apikey": "60352dbc5ad3610fb5bb658c",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw Error(response.statusText);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     //we have the data
+//     console.log(data);
+//     //handle the data
+//     handleData(data);
+//   })
+//   .catch((e) => {
+//     console.error("An Error Occured:", e.message);
+//   });
+
+// fetch(url1, options)
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw Error(response.statusText);
+//     }
+//     return response.json();
+//   })
+//   .then((data1) => {
+//     //we have the data
+//     console.log(data1);
+//     //handle the data
+//     handleData1(data1);
+//   })
+//   .catch((e) => {
+//     console.error("An Error Occured:", e.message);
+//   });
+
+// const urlParams = new URLSearchParams(window.location.search);
+// //in the url grab id and store them under id
+// const id = urlParams.get("id");
+// console.log(id);
+
+const url =
+  "https://semester02t7-00e2.restdb.io/rest/breathe-advice/60352a0e9fc7b60c00017f91";
 
 //The API-Key:
 
@@ -10,66 +60,28 @@ const options = {
 };
 
 fetch(url, options)
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
+  .then((res) => res.json())
   .then((data) => {
     //we have the data
-    console.log(data);
+    // console.log(data);
     //handle the data
-    handleData(data);
+    showEmotion(data);
   })
   .catch((e) => {
     console.error("An Error Occured:", e.message);
   });
 
-fetch(url1, options)
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-  .then((data1) => {
-    //we have the data
-    console.log(data1);
-    //handle the data
-    handleData1(data1);
-  })
-  .catch((e) => {
-    console.error("An Error Occured:", e.message);
-  });
+function showEmotion(emotion) {
+  console.log(emotion);
 
-function handleData(breathe) {
-  breathe.forEach((emotion) => {
-    console.log(emotion);
-    //make a template
-    //grab it
-    const template = document.querySelector("template").content;
-    //clone it
-    const clone = template.cloneNode(true);
-    //populate with data
-    clone.querySelector("h1").textContent = emotion.category;
-    clone.querySelector("h3").textContent = emotion.description;
-    //append it
-    const mainEl = document.querySelector("main");
-    mainEl.appendChild(clone);
-  });
-}
-
-function handleData1(breathe) {
-  breathe.forEach((emotion) => {
-    console.log(emotion);
-    //make a template
-    //grab it
-    //clone it
-    //populate with data
-    //append it
-    //
-  });
+  //populate site
+  document.querySelector(".category").textContent = emotion.category;
+  document.querySelector(".description").textContent = emotion.description;
+  document.querySelector(".title").textContent = emotion.title;
+  document.querySelector(".advice1").textContent = emotion.advice_1;
+  document.querySelector(".advice2").textContent = emotion.advice_2;
+  document.querySelector(".advice3").textContent = emotion.advice_3;
+  document.querySelector("img").src = emotion.img;
 }
 
 // TEXT: category description img
